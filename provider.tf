de5c278c003a13a8f396485a5924a0d1db256291
rafaelalmeidav <rafael.4avv@gmail.com>
2026-02-09 09:33:50 -0300

    feature: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.30.0"
     }
+  }
+  backend "s3" {
+    bucket = "comunidadedevops-rafael"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
